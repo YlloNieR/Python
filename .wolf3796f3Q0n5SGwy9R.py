fe258897cc9e16ba04446b39961ae2c97e6d45de
YlloNieR <30906472+YlloNieR@users.noreply.github.com>
2019-07-24 06:57:21 +0200

    Initial commit

---
+++ .wolf3796f3Q0n5SGwy9R.py
@@ -0,0 +1,4 @@
+x = 17
+y = "3.2"
+message ="Hello, World!"
+z = "1 + 1"
---
+++ Python-Übung.py
@@ -0,0 +1,225 @@
+print("Python-Übungen", '\n',"How to Think Like a Computer Scientist" '\n',"Learning with Python" '\n')
+##Variables
+x = 17
+y = "3.2"
+message = "Hello, World!"
+z = "1 + 1"
+
+print(x, "=", type(x),'\n')
+
+print(y, "=", type(y),'\n')
+
+print(message, "=", type(message),'\n')
+
+print(z, "=", type(z),'\n')
+##Ende Variables
+
+print ('\n')
+
+##Operators and operands
+print("
+print("Operators = \n +,-,*,/, ** = exponentiation \n examples: \n \n hour = 60 \n minute = 59")
+hour = 60
+minute = 59
+print("\n 20+32 =", 20+32, '\n', "hour-1 =", hour-1, '\n', "hour*60+minute =", hour*60+minute, '\n', "5**2 =", 5**2, "= exponentiation" '\n', "(5+9)*(15-7) =",(5+9)*(15-7), '\n')
+
+print("minute/60 =", minute/60, "= integer division")
+print("minute*100/60 =", minute*100/60)
+print("int(minute*100/60) = rounded down = ",int(minute*100/60), '\n')
+
+print("3*Fun =", 3*"Fun", '\n' "fun + fun = ""fun"+"fun")
+
+print("comments with #") #Hier ist ein Kopmmentar
+##Ende Operators and operands
+
+print ('\n')
+
+##Functions
+print("type(32)=", type(32))
+print("type= Funktionsname, (32)= argument von der Funktion,",type(32),"= return value or result")
+
+import math
+print("math.sqrt(2) / 2.0 =",math.sqrt(2) / 2.0, "= Quadratwurzel von 2 durch 2" )
+print ('\n')
+##Funktionen definieren
+print ("First Line", '\n', "Funktion newLine() = Second Line" )
+print ("newLine() =" )
+
+def newLine():
+    print("Second Line")
+newLine() 
+print ('\n')
+
+print ("Funktion threeLine() = Second Line 3 mal untereinander" )
+#def threeLines():
+#    newLine()
+#    newLine()
+#    newLine()
+
+#threeLines()
+#print ('\n')
+
+#print ("Übung",'\n', "As an exercise, write a function called nineLines that uses threeLines to print nine blank lines. How would you print twentyseven new lines?")
+#def threeLines():
+#    print("1")
+#    print("2")
+#    print("3")
+
+#def nineLines():
+#    threeLines()
+#    threeLines()
+#    threeLines()
+
+#def twentysevenNewLines():
+#    nineLines()
+#    nineLines()
+#    nineLines()
+
+#print('\n',"twentysevenNewLines definiert")
+#twentysevenNewLines()
+
+print ('\n')
+
+print("def printTwice(bruce):",'\n'"print (bruce, bruce)")
+def printTwice(bruce):
+    print (bruce, bruce)
+print('\n',"printTwice('Spam') =")
+printTwice('Spam')
+
+print('\n',"def catTwice(part1, part2):",'\n'"cat = part1 + part2 =")
+def catTwice(part1, part2):
+    cat = part1 + part2
+
+printTwice('cat')
+##Ende Funktionen definieren
+
+print ('\n')
+
+##The modulus operator
+print('\n'"quotient = int(7 / 3)")
+quotient = int(7 / 3)
+print ("= ", quotient)
+
+print ('\n'"Rest von 7 / 3")
+remainder = 7 % 3
+print ("= ", remainder)
+##Ende The modulus operator
+
+print ('\n')
+
+##Boolean expressions
+a = (5 == 5)
+print("5 = 5 ...", a)
+
+b = 5 == 6
+print("5 = 6 ...", b)
+
+print("a =",int(a),", b =",int(b),"a>b = ",a>b)
+##Ende Boolean expressions
+
+print ('\n')
+
+##Conditional execution
+c = 5
+if c > 0:
+    print('yes')
+
+print("if ... FIRST STATEMENT",'\n'"  print(...) ... LAST STATEMENT")
+##Ende Conditional execution
+
+print ('\n')
+
+##Alternative execution
+d = 12
+if d%2 == 0:
+    print("Der Rest von", d,"/2", 'ist even 0') #True
+else:
+    print(d, 'is odd')
+##Ende Alternative execution
+
+print ('\n')
+
+##Chained conditionals
+def compare(d,c):
+    if d < c:
+        print (d, "is less than", c)
+    elif d > c:
+        print (d, "is greater than", c)
+    else:
+        print (d, "and", c, "are equal")
+
+compare(d,c)
+print ('\n')
+
+def functionA():
+    print ("functionA was called")
+
+def functionB():
+    print ("functionB was called")
+
+def functionC():
+    print ("functionC was called")
+
+def dispatch(choice):
+    if choice == 'A':
+        functionA()
+    elif choice == 'B':
+        functionB()
+    elif choice == 'C':
+        functionC()
+    else:
+        print("Invalid choice.")
+dispatch("S")
+##Ende Chained conditionals
+
+print ('\n')
+
+##The return statement
+import math
+f = 14
+print("f =",f,"ist nicht kleiner gleich 0 -> return",'\n')
+
+def printLogarithm(f):
+    if f <= 0:
+        print ("Positive numbers only, please.")
+        return #Muss innerhalb einer Funktion sein
+
+    result = math.log(f) #wird ignoriert
+    print ("The log of f is", result) #wird ignoriert
+##Ende The return statement
+
+print ('\n')
+
+##Recursion
+print("Countdown")
+def countdown(n):
+    if n == 0:
+        print ("Blastoff!")
+    else:
+        print (n)
+        countdown(n-1)
+
+countdown(3)
+print("Countdown mit NewLine Funktion")
+print ('\n')
+def newLine(m):
+    if m == 2:
+        print ("2")
+    else:
+        print (m)
+        newLine(m+1)
+
+newLine(1)
+##Ende Recursion
+
+print ('\n')
+
+##Infinite recursion
+def recurse( g ):
+    recurse( g+1 )
+    
+recurse(0)
+##Ende Infinite recursion
+
+
+
