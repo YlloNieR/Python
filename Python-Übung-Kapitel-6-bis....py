9ef8fcfd34ee1473a78f568bb4bbc6688877160e
YlloNieR <30906472+YlloNieR@users.noreply.github.com>
2019-07-25 10:42:52 +0200

    Beginn Kapitel 6

--- Python-Übung-Kapitel-6-bis....py
+++ Python-Übung-Kapitel-6-bis....py
@@ -1,6 +1,6 @@
 ##Iteration  Kapitel 6
 ##Multiple assignment
-print("Multiple assignment =")
+print("##Multiple assignment =\n")
 bruce = 5
 print("bruce erster Aufruf = ",bruce)
 bruce = 7
@@ -18,8 +18,8 @@ print("b =",b,"...b bleibt weiterhin ",b)
 print('\n')
 
 ##The while statement
-print("The while statement =")
-
+print("##The while statement =\n")
+print("countdown mit while =")
 def countdown(n):
     while n > 0: #solange n größer 0
         print(n) #Zeige jede Zahl an
@@ -28,20 +28,68 @@ def countdown(n):
 
 countdown(2) #Funktionsaufruf
 
+print("\nsequence mit while = nicht ausgeführt da infinite loop")
+def sequence(n):
+    while n != 1:  #Loop bis n = 1
+        print(n)  #Zeige solange n an
+    if n%2 == 0: 
+        n = n/2    #dann n/2
+    else:          
+        n = n*3+1  #ansonsten n mal 3 + 1
+print("\nÜbung nLine umschreiben in while")
+def nLines(n):
+    while n > 0:
+        print("a")
+        n = n-1
 
+nLines(2)
 ##Ende The while statement
+print('\n')
+
+##Tables
+print("##Tables =\n")
+import math
+
+Basis = 2.0
+Potenz = 8.0
+Exponent = "x"
+print("Zahlen bestehen aus:\n Basis =",int(Basis),"\n Exponent =",Exponent,"\n Beispiel: ",int(Basis),"hoch",Exponent,"dann ist",Exponent,"ist der Exponent, das Ergebnis ist die Potenz.")
+print(" Der Logarithmus ist dann log von",int(Potenz),"zur Basis",int(Basis),".",'\n')
+x = 1.0
+
+print("print ( x, /t, math.log(x) )") #Beachte / anders \
+print("   x =",x) 
+while x < 10.0:
+    print(x, '\t', math.log(x))
+    x = x + 1.0  # + 1.0 = Potenzwert
+
+print("\nprint ( x, /t, math.log(x)/math.log(2.0) )") #Beachte / anders \
+x = 1.0
+while x < 100.0:
+    print(x, '\t', math.log(x)/math.log(2.0))
+    x = x * 2.0  # * 2.0 = Potenzwert
+
+print("\nÜbung log") 
+Basis = 2.0
+Exponent = 16
+while Basis < 65536:
+    print(Basis, '\t', math.log(Basis)/math.log(2.0))
+    Basis = Basis * 2.0  
+
+print("\nÜbung Schreibweise Tabelle") 
+print("\nproduces")
+print('\t''\t'"this")
+print(3*'\t',"output.")
+##Ende Tables
 
 print('\n')
 
-##
+##Two-dimensional tables
+print("##Two-dimensional tables =\n")
 
-##Ende 
 
-print('\n')
+##Ende Two-dimensional tables
 
-##
-
-##Ende 
 print('\n')
 
 ##
