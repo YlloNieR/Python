49a458a30e781d2c4fb46fa946ef49fcc03ed395
YlloNieR <30906472+YlloNieR@users.noreply.github.com>
2019-08-01 21:38:11 +0200

    update

--- asd.py
+++ asd.py
@@ -3,3 +3,36 @@ print("##Classes and functions")
 ##Time
 print("##Time = ")
 
+class Time:
+    pass
+
+time = Time()
+time.hours = 11
+time.minutes = 59
+time.seconds = 30
+
+print("Stunde =",time.hours," Minute = ", time.minutes," Sekunden = ", time.seconds)
+
+print("\nAs an exercise, write a function printTime that takes a Time object as an argument and prints it in the form hours:minutes:seconds.")
+
+def printTime(hh,mm,ss):
+    class Time:
+        pass
+    time = Time()
+    time.hh = hh
+    time.mm = mm
+    time.ss = ss
+    print(time.hh,":",time.mm,":",time.ss," Uhr")
+
+printTime(22,13,12)
+##Ende Time
+##Ende Classes and functions
+
+##Classes and methods
+print("##Classes and methods")
+##Time
+print("##Object-oriented features = ")
+
+
+##Ende Classes and methods
+
