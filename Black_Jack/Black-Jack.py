9ae987dcba01fec52a33a3df845278b9776a3af2
YlloNieR <30906472+YlloNieR@users.noreply.github.com>
2019-08-12 22:29:40 +0200

    update

---
+++ Black_Jack/Black-Jack.py
@@ -0,0 +1,28 @@
+print("\t Black Jack")
+'''
+312 Karten
+52 Blatt 13 Karten
+K, Q, J
+
+Croupier
+bis zu sieben Spieler 
+
+'''
+blattDiamonds = ("Diamond")
+blattHearts = {"Heart"}
+blattSpades = {"Spade"}
+blattSquares = {"Square"}
+
+number = {"Diamond":2,"Diamond":3,"Diamond":4,"Diamond":5,"Diamond":6,"Diamond":7,"Diamond":8,"Diamond":9,"Diamond":10,"Diamond":"J","Diamond":"Q","Diamond":"K","Diamond":"A"}
+
+
+print(number)
+print(number.keys)
+print(number.items)
+
+
+
+
+
+
+
