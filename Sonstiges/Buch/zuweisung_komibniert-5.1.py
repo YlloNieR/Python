8881d9aaa6815bf73f6a235df71e50a04fd96492
YlloNieR <30906472+YlloNieR@users.noreply.github.com>
2019-09-12 18:46:13 +0200

    zuweisung komibiniert

---
+++ Sonstiges/Buch/zuweisung_komibniert-5.1.py
@@ -0,0 +1,27 @@
+# Kombinierte Zuweisungsoperatoren für Zahlen
+x = 12
+print(x, "Ausgangswert x")
+x += 3      # Erhöhen von x
+print(x, "Erhöhen von x")
+x -= 9      # Vermindern von x
+print(x, "Vermindern von x")
+x **= 2     # Quadrieren von x
+print(x, "Quadrieren von x")
+x *= 3      # Verdreifachen von x
+print(x, "Verdreifachen von x")
+x //= 7     # Ganzzahliges Teilen von x mit 7 -> Abschneiden der Nachkommastellen
+print(x, "Ganzzahliges Teilen von x mit 7")
+x /= 4      # Teilen von x
+print(x, "Teilen von x")
+x %= 2      # Dividieren, Rest berechnen
+print(x, "Dividieren, Rest berechnen")
+
+# kombinierte Zuweisungsoperatoren für Zeichenketten
+t = " hallo "
+print(t, "t = hallo")
+
+t += " python "  # Anhängen an t
+print(t, "Anhängen an t")
+
+t *= 3           # Verdreifachen von t
+print(t, "Verdreifachen von t")
