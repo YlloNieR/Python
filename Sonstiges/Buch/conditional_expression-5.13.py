10e067d7f731add8b26bee7ee6b8e3a27cae1ac3
YlloNieR <30906472+YlloNieR@users.noreply.github.com>
2019-09-15 13:33:04 +0200

    conditional expression

---
+++ Sonstiges/Buch/conditional_expression-5.13.py
@@ -0,0 +1,10 @@
+x = -12
+y = 15
+
+# Ausdruck zur Ausgabe
+max = x if x > y else y
+# variable max enthält den Wert von x, falls x größer als y ist; anderefalls erhält die Variable max den Wert von y
+print(max)
+
+# Ausdruck zur Ausgbae
+print("positiv" if x > 0 else "negativ oder o")
