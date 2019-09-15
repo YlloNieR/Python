b03d165d486d20178d3b7eecc1159e79591373c9
YlloNieR <30906472+YlloNieR@users.noreply.github.com>
2019-09-15 15:33:13 +0200

    fehler code

---
+++ Sonstiges/Buch/fehler_code-5.18.py
@@ -0,0 +1,4 @@
+x 12            # SyntaxError: invalid Syntax ("=" fehlt)
+
+if x > 10       # SyntaxError: invalid Syntax (":" fehlt)
+    print(x     # SyntaxError: unexpected EOF while parsing (")" fehlt) EOF... End of File
