c875d3c17008aface69575d9e9956773628a53d2
YlloNieR <30906472+YlloNieR@users.noreply.github.com>
2019-09-16 21:37:42 +0200

    asd

--- Sonstiges/Buch/chr_ord-5.34.py
+++ Sonstiges/Buch/chr_ord-5.34.py
@@ -1,23 +1,23 @@
 # Ziffern
-for i in range(48, 58):
+for i in range(48, 58):         # Unicode-Zahlen 48-57 = Ziffern 0-9
     print(chr(i), end="")
 print()
 
 # große Buchstaben
-for i in range(65, 91):
+for i in range(65, 91):         # Unicode-Zahlen 65-90 = große Buchstaben
     print(chr(i), end="")
 print()
 
 # kleine Buchstaben
-for i in range(97, 123):
+for i in range(97, 123):        # Unicode-Zahlen 97-122 = kleine Buchstaben
     print(chr(i), end="")
 print()
 
 # Codenummern
 for z in "Robinson":
-    print(ord(z), end=" ")
+    print(ord(z), end=" ")      # Codenummern der Unicode-Zahlen von "Robinson"
 print()
 
 # Verschoben
-for z in "Robinson":
+for z in "Robinson":            # Verschlüselung anhand der Umwandlung von Unicode der Codenummern + 1 vom Wort Robinson
     print(chr(ord(z)+1), end="")
---
+++ Sonstiges/Buch/reversed_sorted-5.35.py
@@ -0,0 +1,18 @@
+# Originale
+z = "Robinson"
+print(z)
+t = [4, 12, 6, -2]
+print(t)
+
+# umgedreht
+r = reversed(z)
+
+for x in r:
+    print(x, end="")
+print()
+
+# sortierte Listen
+s1 = sorted(z)
+print(s1)
+s2 = sorted(t)
+print(s2)
