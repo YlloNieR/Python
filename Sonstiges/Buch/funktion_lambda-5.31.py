a17b6af7ec1b1f8e75a6e9cf5e1e0024d72a82ef
YlloNieR <30906472+YlloNieR@users.noreply.github.com>
2019-09-16 20:52:25 +0200

    CheatSheet, lamda,parameter

---
+++ Sonstiges/Buch/funktion_lambda-5.31.py
@@ -0,0 +1,14 @@
+# Funktionsdefinitionen
+def mal(x, y): return x*y
+# alternative Schreibweise =
+# mal = lambda x,y: x*y
+
+
+def plus(x, y): return x+y
+# alternative Schreibweise =
+# plus = lambda x,y: x+y
+
+
+# Funktionsaufrufe
+print(mal(5, 3))
+print(plus(4, 7))
