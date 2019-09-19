b12689a66cf950a096877dfe15666076b6ec7878
YlloNieR <30906472+YlloNieR@users.noreply.github.com>
2019-09-19 18:40:39 +0200

    lesen obst.txt und schreiben

---
+++ Sonstiges/Buch/lesen_beliebig_8_13.py
@@ -0,0 +1,25 @@
+import sys
+
+# Zugriffsversuch
+try:
+    d = open("obst_8_12.txt")
+except:
+    print("Dateizugriff nicht erfolgreich")
+    sys.exit(0)
+
+# Gezieltes Lesen
+for i in range(1, 4):
+    # Nr Lesen
+    d.seek(48*i)
+    nr = int(d.read(4))
+
+    # EP Lesen
+    d.seek(20 + 48*i)
+    ep = float(d.read(8))
+
+    # Ausgabe
+    print("Artikel Nr: ", nr, ", EP: ", ep)
+
+
+# Schließe der Datei
+d.close()
