f584a00931b617e1f4ee6fe093c871d90bbacff3
YlloNieR <30906472+YlloNieR@users.noreply.github.com>
2019-09-19 19:45:36 +0200

    datei namen und inhalt überprüfen

---
+++ Sonstiges/Buch/datei_liste_8_18.py
@@ -0,0 +1,24 @@
+import glob
+
+
+# Liste der Dateien
+dateiliste = glob.glob("schr*.py")
+
+# Jedes Elemnt der Liste durchsuchen
+for datei in dateiliste:
+    # Zugriffsversuch
+    try:
+        d = open(datei)
+    except:
+        print("Dateizugriff nicht erfolgreich")
+        continue
+
+    # Text einlesen
+    gesamtertext = d.read()
+
+    # Zugriff beenden
+    d.close()
+
+    # Suchtext suchen
+    if gesamtertext.find("obst") != -1:
+        print(datei)
