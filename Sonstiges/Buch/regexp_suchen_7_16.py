e4e972e3a10367b74356f1c9a03bb66f0019212d
YlloNieR <30906472+YlloNieR@users.noreply.github.com>
2019-09-19 15:42:32 +0200

    sound

--- Sonstiges/Buch/regexp_suchen_7_16.py
+++ Sonstiges/Buch/regexp_suchen_7_16.py
@@ -4,30 +4,30 @@ import re
 # 1: Exakter Text
 tx = "Haus und Maus und Laus"
 print(tx)
-erg = re.findall("Maus",tx)
+erg = re.findall("Maus", tx)
 print("1: ", erg)
 
 # 2: Wahl zwischen bestimmten Zeichen
-erg = re.findall("[HM]aus",tx)
+erg = re.findall("[HM]aus", tx)
 print("2: ", erg)
 
 # 3: Alle Buchstaben aus Bereich
-erg= re.findall("[L-M]aus",tx)
+erg = re.findall("[L-M]aus", tx)
 print("3: ", erg)
 
 # 4: Alle Buchstaben nicht aus Bereich
-erg = re.findall("[^L-M]aus",tx)
+erg = re.findall("[^L-M]aus", tx)
 print("4:", erg)
 
 # 5: Belibieges Zeichen
-erg = re.findall(".aus",tx)
+erg = re.findall(".aus", tx)
 print("5: ", erg)
 
 # 6: Suchbegriff nur am Anfang des Texts
-erg = re.findall("^.aus",tx)
+erg = re.findall("^.aus", tx)
 print("6: ", erg)
 
 # 7: Suchbegriff nur am Ende des Textes
-erg = re.findall(".aus$",tx)
+erg = re.findall(".aus$", tx)
 print("7: ", erg)
 print()
