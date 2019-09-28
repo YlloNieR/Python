8cf687f68b1b90409dc2cdc94e53b922c92a2b17
YlloNieR <30906472+YlloNieR@users.noreply.github.com>
2019-09-28 19:43:58 +0200

    beginn 9. Kap

---
+++ Sonstiges/Buch/url_lesen_9_2.py
@@ -0,0 +1,21 @@
+import sys
+import urllib.request
+
+
+# Verbindung zu einer URL
+try:
+    u = urllib.request.urlopen("http://localhost/Python36/url_lesen_9_1.htm")
+
+except:
+    print("Fehler")
+    sys.exit(0)
+
+# Liest alle Zeilen in eine Liste
+li = u.readlines()
+
+# Schliesst die Verbindung
+u.close()
+
+# Ausgabe der Liste
+for element in li:
+    print(element)
