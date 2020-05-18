0fa521fd8e5e15202ff942d40d3d0e6e11f26f8b
YlloNieR <30906472+YlloNieR@users.noreply.github.com>
2020-05-18 21:52:19 +0200

    erste Muster

--- portfolio/pf_otpr_draw_bw_camouflage.py
+++ portfolio/pf_otpr_draw_bw_camouflage.py
@@ -4,8 +4,9 @@ import turtle
 
 Bw_camouflage = tk.Tk()
 Bw_camouflage.title('Turtle Zeichnung')
-Bw_camouflage.geometry("290x225+1800+100")  # großer Monitor
-# Bw_camouflage.geometry("290x225+100+0")  # kleiner Monitor
+Bw_camouflage.geometry("300x300+1800+5")  # großer Monitor
+# Bw_camouflage.geometry("310x225+50+0")  # kleiner Monitor
+turtle.Screen().setup(width=1600,height=1000,startx=1800,starty=360)
 
 def end():
     Bw_camouflage.destroy()
@@ -68,7 +69,128 @@ def pic1():
     turtle.done()
 
 
-def pic2():
+def EINS():    
+    #turtle.setheading(0)
+    # 0 - east
+    # 45
+    # 90 - north
+    # 135
+    # 180 - west
+    # 225
+    # 270 - south
+    # 310
+    linie = "white"
+    colorlisteGreen = ["#383F2D","#444E36","#49563C","#505A35","#54633A","#636F4B","#727655","#75855E"]
+    colorlisteBrown = ["#6B5D38","#59502F","#6E5634","#3D351E","#463A22","#433824","#665134","#302917"]
+    colorlisteYellow = ["#78744F","#BDB77D","#C6C285","#9A8C5B","#A69965","#4F4B30","#484B30","#CAC989"]
+
+    x = 0
+    turtle.color(linie,colorlisteGreen[x])
+    turtle.begin_fill()
+    for i in range(2):
+        turtle.color(linie,colorlisteGreen[x])
+        turtle.forward(100)
+        turtle.left(120)
+    turtle.end_fill()
+    turtle.goto(0,0)
+    turtle.setheading(60)
+
+    x = x + 1
+    turtle.color(linie,colorlisteGreen[x])
+    turtle.begin_fill()
+    for i in range(2):
+        turtle.color(linie,colorlisteGreen[x])
+        turtle.forward(100)
+        turtle.left(120)
+    turtle.end_fill()
+    turtle.setheading(120)
+    turtle.goto(0,0)
+
+    x = x + 1
+    turtle.color(linie,colorlisteGreen[x])
+    turtle.begin_fill()
+    for i in range(2):
+        turtle.color(linie,colorlisteGreen[x])
+        turtle.forward(100)
+        turtle.left(120)
+    turtle.end_fill()
+    turtle.goto(0,0)
+    turtle.setheading(180)
+
+    x = x + 1
+    turtle.color(linie,colorlisteGreen[x])
+    turtle.begin_fill()
+    for i in range(2):
+        turtle.color(linie,colorlisteGreen[x])
+        turtle.forward(100)
+        turtle.left(120)
+    turtle.end_fill()
+    turtle.goto(0,0)
+    turtle.setheading(240)
+
+    x = x + 1
+    turtle.color(linie,colorlisteGreen[x])
+    turtle.begin_fill()
+    for i in range(2):
+        turtle.color(linie,colorlisteGreen[x])
+        turtle.forward(100)
+        turtle.left(120)
+    turtle.end_fill()
+    turtle.goto(0,0)
+    turtle.setheading(300)
+
+    x = x + 1
+    turtle.color(linie,colorlisteGreen[x])
+    turtle.begin_fill()
+    for i in range(2):
+        turtle.color(linie,colorlisteGreen[x])
+        turtle.forward(100)
+        turtle.left(120)
+    turtle.end_fill()
+    turtle.goto(0,0)
+    turtle.setheading(300)
+
+
+def ZWEI():
+    #turtle.setheading(0)
+    # 0 - east
+    # 45
+    # 90 - north
+    # 135
+    # 180 - west
+    # 225
+    # 270 - south
+    # 310
+    linie = "white"
+    colorlisteGreen = ["#383F2D","#444E36","#49563C","#505A35","#54633A","#636F4B","#727655","#75855E"]
+    colorlisteBrown = ["#6B5D38","#59502F","#6E5634","#3D351E","#463A22","#433824","#665134","#302917"]
+    colorlisteYellow = ["#78744F","#BDB77D","#C6C285","#9A8C5B","#A69965","#4F4B30","#484B30","#CAC989"]
+
+    x = 0
+    x = x + 1
+    turtle.color(linie,colorlisteBrown[x])
+    turtle.begin_fill()
+    turtle.forward(100)
+    turtle.setheading(0)
+    turtle.forward(100)
+    turtle.setheading(120)
+    turtle.forward(100)
+    turtle.setheading(240)
+    turtle.forward(100)
+    turtle.end_fill()
+
+    
+
+def DREI():
+    #turtle.setheading(0)
+    # 0 - east
+    # 45
+    # 90 - north
+    # 135
+    # 180 - west
+    # 225
+    # 270 - south
+    # 310
     linie = "white"
     colorlisteGreen = ["#626844","#424C34","#373E2C","#546241","#5B6644"]
 
@@ -76,57 +198,14 @@ def pic2():
     turtle.begin_fill()
     for i in range(2):
         turtle.color(linie,colorlisteGreen[0])
-        turtle.forward(500)
-        turtle.left(120)
+        turtle.forward(100)
+        turtle.setheading(120)
     turtle.end_fill()
     turtle.goto(0,0)
     turtle.setheading(60)
-
-    turtle.color(linie,colorlisteGreen[0])
-    turtle.begin_fill()
-    for i in range(2):
-        turtle.color(linie,colorlisteGreen[1])
-        turtle.forward(500)
-        turtle.left(120)
-    turtle.end_fill()
-    turtle.setheading(180)
-    turtle.goto(0,0)
-
-
-def dreieckmuster():    
-    linie = "white"
-    colorlisteGreen = ["#626844","#424C34","#373E2C","#546241","#5B6644"]
-
-    #for x in colorlisteGreen:
-    
-    # e-nw-s
-    turtle.color(linie,colorlisteGreen[0])
-    turtle.begin_fill()
-    for i in range(2):
-        turtle.forward(50)
-        turtle.left(120)
-    turtle.end_fill()
-
-    # sw-e
-    turtle.color(linie,colorlisteGreen[1])
-    turtle.begin_fill()    
-    for i in range(2):
-        turtle.forward(100)
-        turtle.setheading(45)
-    turtle.end_fill()
-    turtle.done()
-
-
-
-def dreieckerstellen():
-    turtle.color("white","#48553B")
-    turtle.begin_fill()
-    for i in range(3):
-        turtle.forward(-50)
-        turtle.setheading(-120)
-    turtle.end_fill()
         
-
+def allClear():
+    turtle.clear()
 
 # Frame 1
 rahmenreihe1 = tk.Frame(Bw_camouflage)
@@ -134,11 +213,14 @@ bback = tk.Button(rahmenreihe1, width=10, text="Zurück", command=back)
 bende = tk.Button(rahmenreihe1, text='Ende', command=end)
 brebootBw_camouflage = tk.Button(
     rahmenreihe1, text='Neustart', command=rebootBw_camouflage)
+ballClear = tk.Button(
+    rahmenreihe1, text='Clear', command=allClear)
 
 rahmenreihe1.pack(fill='x',padx=10)
 bback.pack(padx=10, pady=5)
 bende.pack(padx=10, pady=5)
 brebootBw_camouflage.pack(padx=10, pady=5)
+ballClear.pack(padx=10, pady=5)
 
 
 
@@ -190,9 +272,9 @@ barrowSE.pack(padx=5, pady=5)
 spaltenreihePic = tk.Frame(Bw_camouflage)
 spaltenreihePic.pack(fill='y',side="left", padx=5)
 
-bpic1 = tk.Button(spaltenreihePic, width="6", text='Eins', command=dreieckerstellen, fg="#4287f5")
-bpic2 = tk.Button(spaltenreihePic, text='pic2', command=pic2, fg="#a442f5")
-bpic3 = tk.Button(spaltenreihePic, text='Muster', command=dreieckmuster, fg="#2b6318")
+bpic1 = tk.Button(spaltenreihePic, width="6", text='EINS', command=EINS, fg="#4287f5")
+bpic2 = tk.Button(spaltenreihePic, text='ZWEI', command=ZWEI, fg="#a442f5")
+bpic3 = tk.Button(spaltenreihePic, text='DREI', command=DREI, fg="#2b6318")
 
 bpic1.pack(padx=5, pady=5)
 bpic2.pack(padx=5, pady=5)
